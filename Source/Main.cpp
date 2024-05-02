a0006e8c94a6b8d80000494f73c95099aa8bad6f
SicdeX <sicdex@gmail.com>
2024-05-02 23:50:05 +0600

    see desc
    - Fixed send_input mouse3 to key F
    - Added check only hold LSHIFT + F

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -9,81 +9,71 @@
 #include <string>
 using namespace std;
 
-int max_prepared_time = 5; /** sec */
+string gameWindowName = "Tiger - 64"; // window game name title
 
+int keyCode = 0x46; // https://learn.microsoft.com/ru-ru/windows/win32/inputdev/virtual-key-codes
+int max_prepared_time = 2; /** sec */
+int delay_check_window = max_prepared_time * 1000; /** sec */
 int counts_attempts_find = 0;
 int max_attempts_find_window = 10;
 
 bool console_clear_setup = false;
 bool console_clear_game_found = false;
-string gameWindowName = "Tiger - 64";
 
 void send_input() {
-
-    INPUT ip;
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0;
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
-
-    // https://docs.microsoft.com/ru-ru/windows/win32/inputdev/virtual-key-codes
-    ip.ki.wVk = 0x04; // MOUSE3 hotkey
-    ip.ki.dwFlags = 0;
-    SendInput(1, &ip, sizeof(INPUT));
-
-    ip.ki.dwFlags = KEYEVENTF_KEYUP;
-    SendInput(1, &ip, sizeof(INPUT));
+    INPUT inputs[2] = {};
+    ZeroMemory(inputs, sizeof(inputs));
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = keyCode;
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = keyCode;
+    inputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+    UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    if (uSent != ARRAYSIZE(inputs)) {
+        printf("SendInput failed: 0x%x\n", HRESULT_FROM_WIN32(GetLastError()));
+    }
 }
 
-string GetActiveWindowTitle()
-{
+string GetActiveWindowTitle() {
     char a[256] = {};
     HWND hw = GetForegroundWindow();
     GetWindowTextA(hw, a, sizeof(a));
-    return string(a);
+    return (string)a;
 }
 
 void LaunchedInfo() {
     printf("- Launched...\n");
+    printf("- Use LSHIFT + F.\n");
     printf("- Use 'Escape' for exit.\n");
-    printf("- if not working set key MOUSE3 (ping settings)\n");
 }
 
-int main()
-{
-    printf("- Please await %ds...\n", max_prepared_time);
-    printf("- Set key ping MOUSE3 in game\n");
+bool IsHoldKeyState() {
+    return (GetAsyncKeyState(keyCode) & 0x8000) && (GetAsyncKeyState(VK_LSHIFT) & 0x8000);
+}
 
+int main() {
+    printf("- Please await %ds...\n", max_prepared_time);
     Sleep(max_prepared_time * 1000);
     system("CLS");
     console_clear_setup = true;
-
     LaunchedInfo();
 
-    while (1) {
-
-        /** user exit */
+    while (true) {
         if (counts_attempts_find >= max_attempts_find_window || (GetAsyncKeyState(VK_ESCAPE) & 1)) {
             exit(0);
             break;
         }
-
-        auto isFocus = GetActiveWindowTitle().compare(gameWindowName) == 1;
-
-        if (!isFocus) {
-
+        if (GetActiveWindowTitle().c_str() != gameWindowName) {
             if (!console_clear_game_found) {
                 system("CLS");
                 console_clear_game_found = true;
             }
-
             printf("- Game window not found, open or expand the game...\n- App will close after [%d/%d]\n", counts_attempts_find + 1, max_attempts_find_window);
             console_clear_setup = false;
             counts_attempts_find++;
-            Sleep(2000);
+            Sleep(delay_check_window);
             continue;
         }
-
         if (!console_clear_setup) {
             system("CLS");
             LaunchedInfo();
@@ -91,11 +81,11 @@ int main()
             console_clear_game_found = false;
             counts_attempts_find = 0;
         }
-
-        for (size_t i = 0; i < 7; i++)
-            send_input();
-
+        if (IsHoldKeyState()) {
+            for (size_t i = 0; i < 7; i++)
+                send_input();
+        }
         Sleep(1);
     }
-    return 0; /** n exit*/
+    return 0;
 }
